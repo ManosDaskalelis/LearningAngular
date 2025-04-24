6e8f4a04863c2b7246d957334ea6d6baf42dcf11
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2025-04-25 00:05:34 +0300

    First commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-users',
+  imports: [],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent {
+ selecterdUser = DUMMY_USERS[randomIndex];
+}
