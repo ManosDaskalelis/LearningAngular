442c0e027414bdf065ac29872901f5b074c779b4
Manos Daskalelis <Manolisdaskalelis@gmai.com>
2025-04-30 00:19:04 +0300

    signal input example

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input} from '@angular/core';
-import { User } from '../models/user.model';
+import { Component, Input, input} from '@angular/core';
+// import { User } from '../models/user.model';
 // import { DUMMY_USERS } from './dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,11 +11,16 @@ import { User } from '../models/user.model';
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
-  @Input() id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  // @Input({required: true}) id!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+  id = input.required<string>();
+  
   // selecterdUser = signal(DUMMY_USERS[randomIndex]);
   // imagePath = computed(() => 'assets/users/' + this.selecterdUser().avatar);
+
 
 
    get imagePath() {
