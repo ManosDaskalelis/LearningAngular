242dff35cbbf621ae1b8cc949d14159beea83f38
DASKALELIS Emmanouil <DaskalelisE@unisystems.gr>
2025-07-22 13:11:19 +0300

    proj3 added

--- proj2/01-starting-project/src/app/app.module.ts
+++ proj2/01-starting-project/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { SharedModule } from "./shared/shared.module";
+
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [],
-    exports: [] 
+    declarations: [AppComponent, HeaderComponent, UserComponent],
+    bootstrap: [AppComponent],
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {
-  // This module is intentionally left empty as it is not needed in this standalone component setup.
 }
