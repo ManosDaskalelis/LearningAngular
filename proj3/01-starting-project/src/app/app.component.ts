c7e482a0e6d7ea1f52d4fe0e489cbfe51ea9e306
DASKALELIS Emmanouil <DaskalelisE@unisystems.gr>
2025-07-22 16:51:32 +0300

    Currency pipe | currency

--- proj3/01-starting-project/src/app/app.component.ts
+++ proj3/01-starting-project/src/app/app.component.ts
@@ -2,14 +2,27 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentData } from './user-input/investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  results: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[] = [];
+
+
+
+
   onCalculateInvestmentResults(data: InvestmentData) {
     const annualData = [];
     let investmentValue = data.initialInvestment;
@@ -21,16 +34,16 @@ export class AppComponent {
       const totalInterest =
         investmentValue - data.annualInvestment * year - data.initialInvestment;
       annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: data.annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
+          year: year,
+          interest: interestEarnedInYear,
+          valueEndOfYear: investmentValue,
+          annualInvestment: data.annualInvestment,
+          totalInterest: totalInterest,
+          totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
       });
     }
-    console.log(annualData);
-    return annualData;
+    this.results = annualData;
+    return this.results;
   }
 
 
