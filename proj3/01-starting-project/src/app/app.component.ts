73cbf00c5ee096b48fb75a1013b4c47fe0099568
DASKALELIS Emmanouil <DaskalelisE@unisystems.gr>
2025-07-22 15:21:18 +0300

    proj3 training

--- proj3/01-starting-project/src/app/app.component.ts
+++ proj3/01-starting-project/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentData } from './user-input/investment-input.model';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,29 @@ import { UserInputComponent } from './user-input/user-input.component';
   imports: [HeaderComponent, UserInputComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  onCalculateInvestmentResults(data: InvestmentData) {
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + data.annualInvestment;
+      const totalInterest =
+        investmentValue - data.annualInvestment * year - data.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
+      });
+    }
+    console.log(annualData);
+    return annualData;
+  }
+
+
+}
