d2793e4e932e5eca24f1fc9d33265bce2a7b6234
DASKALELIS Emmanouil <DaskalelisE@unisystems.gr>
2025-07-24 13:38:47 +0300

    proj3

--- proj3/01-starting-project/src/app/app.component.ts
+++ proj3/01-starting-project/src/app/app.component.ts
@@ -1,50 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from './user-input/user-input.component';
-import { InvestmentData } from './user-input/investment-input.model';
-import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  results: {
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[] = [];
 
 
 
 
-  onCalculateInvestmentResults(data: InvestmentData) {
-    const annualData = [];
-    let investmentValue = data.initialInvestment;
 
-    for (let i = 0; i < data.duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
-      investmentValue += interestEarnedInYear + data.annualInvestment;
-      const totalInterest =
-        investmentValue - data.annualInvestment * year - data.initialInvestment;
-      annualData.push({
-          year: year,
-          interest: interestEarnedInYear,
-          valueEndOfYear: investmentValue,
-          annualInvestment: data.annualInvestment,
-          totalInterest: totalInterest,
-          totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
-      });
-    }
-    this.results = annualData;
-    return this.results;
-  }
 
 
 }
--- proj3/01-starting-project/src/app/investment-results/investment-results.component.ts
+++ proj3/01-starting-project/src/app/investment-results/investment-results.component.ts
@@ -1,20 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject} from '@angular/core';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  results = input<{
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number,
-  }[]>();
+  private investmentService = inject(InvestmentService);
+
+  // get results() {
+  //   return this.investmentService.results();
+  // }
+
+  // results = computed(() => this.investmentService.results());
+
+  results = this.investmentService.results.asReadonly();
 }
