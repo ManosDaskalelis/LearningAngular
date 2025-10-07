ec6cb30964187e5aacd07e5d464564a56770165e
DASKALELIS Emmanouil <DaskalelisE@unisystems.gr>
2025-10-07 17:09:50 +0300

    host event

--- proj5/01-starting-project/src/app/shared/control/control.component.ts
+++ proj5/01-starting-project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,21 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 
 export class ControlComponent {
+  // @HostBinding('class') className = 'control';
+  // @HostListener('click')  onClick() {
+  // alert(`You clicked on ${this.labelText}`);
+  //}
   labelText = input.required<string>();
+
+  
+  onClick() {
+    alert(`You clicked on ${this.labelText}`);
+  }
+
 }
